45f2dcfcbd8ad85b7f3ac96c74987c0607186c97
abaga129 <ethanepr@hotmail.com>
2017-03-18 11:54:14 -0500

    Update config

--- config.py
+++ config.py
@@ -2,9 +2,12 @@
 # THIS FILE MUST BE CONFIGURED BEFORE USING ANY OF THE MODULES #
 # Below are variables which indicate which pins on the PI will #
 # be used.                                                     #
+# - Replace "" with actual pin # without quotes.
 ################################################################
 
 import RPi.GPIO as GPIO
+
+mode = "BOARD" #define the pin numbering system to be used. (BOARD, BCM)
 
 # L298N Pins
 A_FORWARD = ""
@@ -30,6 +33,25 @@ def setup_pin(pinNo, mode):
     try:
       if mode:
         GPIO.setup(pinNo, GPIO.IN)
+      else:
+        GPIO.setup(pinNo, GPIO.OUT)
     except:
       print "Error setting up pin ", pinNo
-    
+      
+if mode == "BOARD":
+  GPIO.setmode(GPIO.BOARD)
+else:
+  GPIO.setmode(GPIO.BCM) 
+
+setup_pin(A_FORWARD, 0)
+setup_pin(A_REVERSE, 0)
+setup_pin(B_FORWARD, 0)
+setup_pin(B_REVERSE, 0)
+setup_pin(TRIG1, 0)
+setup_pin(TRIG2, 0)
+setup_pin(TRIG3, 0)
+setup_pin(TRIG4, 0)
+setup_pin(TRIG5, 0)
+setup_pin(SERVO1, 0)
+setup_pin(SERVO2, 0)
+setup_pin(SERVO3, 0)
