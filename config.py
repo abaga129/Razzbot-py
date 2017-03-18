554f1f630587c5333c6b313dd115df2494bffc97
Ethan Reker <ethanepr@hotmail.com>
2017-03-17 23:06:17 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+################################################################
+# THIS FILE MUST BE CONFIGURED BEFORE USING ANY OF THE MODULES #
+# Below are variables which indicate which pins on the PI will #
+# be used.                                                     #
+################################################################
+
+import RPi.GPIO as GPIO
+
+# L298N Pins
+A_FORWARD = ""
+A_REVERSE = ""
+B_FORWARD = ""
+B_REVERSE = ""
+
