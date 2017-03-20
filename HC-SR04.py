4f04f5df8b2c60e851bf80f054b1292f93c80530
Ethan Reker <ethanepr@hotmail.com>
2017-03-20 16:18:18 -0500

    Update HC-SR04.py

--- HC-SR04.py
+++ HC-SR04.py
@@ -5,4 +5,33 @@
 # allow simple reading of it.                           #
 #########################################################
 
+import threading
 import conf
+
+class HCSR04(threading.Thread):
+  TRIG = 0
+  ECHO = 0
+  name = ""
+  distance = 0
+  keep = True
+  
+  def __init__(self, TRIG, ECHO, name):
+    threading.Thread.__init__(self)
+    self.TRIG = TRIG
+    self.ECHO = ECHO
+    self.name = name
+             
+  def run(self):
+    while True:
+      
+      
+      
+  #Do not call this until conf.initialize() has been called in main program.
+  def read(self):
+    conf.thread_lock.accquire()
+    ret = distance
+    conf.thread_lock.release()
+    return ret
+    
+  
+  
