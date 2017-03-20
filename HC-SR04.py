07679a87a69739fbfaabaae86f21de2e5f84455a
Ethan Reker <ethanepr@hotmail.com>
2017-03-20 15:00:27 -0500

    Create HC-SR04.py

---
+++ HC-SR04.py
@@ -0,0 +1,8 @@
+#########################################################
+# HC-SR04.py                                            #
+# Written by Ethan Reker 2017                           #
+# This module is intended to setup the sensor and       #
+# allow simple reading of it.                           #
+#########################################################
+
+import conf
