62e407c3522fcf86a9e016be64c23b43c56aa10e
Ethan Reker <ethanepr@hotmail.com>
2017-03-20 19:48:09 -0500

    Update HC-SR04.py

--- HC-SR04.py
+++ HC-SR04.py
@@ -22,16 +22,34 @@ class HCSR04(threading.Thread):
     self.name = name
              
   def run(self):
-    while True:
+    keep = True
+    while keep==True:
+      GPIO.output(TRIG, GPIO.LOW)
+      time.sleep(2)
       
+      GPIO.output(TRIG, GPIO.HIGH)
+      time.sleep(0.00001)
+      GPIO.output(TRIG, GPIO.LOW)
       
+      GPIO.wait_for_edge(ECHO, GPIO.RISING)
+      start_time = time.time()
+      GPIO.wait_for_edge(ECHO, GPIO.FALLING)
+      end_time = time.time()
+      
+      duration = start_time - end_time
+      dist = duration * 17150
+      conf.thread_lock.acquire()
+      distance = dist
+      conf.thread_lock.release()
       
   #Do not call this until conf.initialize() has been called in main program.
   def read(self):
-    conf.thread_lock.accquire()
+    conf.thread_lock.acquire()
     ret = distance
     conf.thread_lock.release()
     return ret
-    
+  
+  def stop(self):
+    keep = False
   
   
