b1584e6c2f6430865e884a9299d993cf1459b2ff
abaga129 <abaga1299@gmail.com>
2017-03-21 15:42:21 +0000

    Fix HC-SR04_test.py

--- HC-SR04_test.py
+++ HC-SR04_test.py
@@ -6,7 +6,7 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 print "Using BOARD numbering system for GPIO pins."
 
 TRIG = raw_input("Enter Pin Number that is connected to TRIG: ")
@@ -14,30 +14,35 @@ ECHO = raw_input("Enter Pin Number that is connected to ECHO: ")
 
 TRIG = int(TRIG)
 ECHO = int(ECHO)
+GPIO.cleanup(TRIG)
+GPIO.cleanup(ECHO)
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 cont = ""
 while cont!="n":
   GPIO.output(TRIG, GPIO.LOW)
-  time.sleep(2)
+  time.sleep(1)
   
   GPIO.output(TRIG, GPIO.HIGH)
   time.sleep(0.0001)
   GPIO.output(TRIG, GPIO.LOW)
   
-  print "Waiting for rising edge."
-  GPIO.wait_for_edge(ECHO, GPIO.RISING)
+  #print "Waiting for edge."
+  GPIO.wait_for_edge(ECHO, GPIO.BOTH, timeout=1000)
   start_time = time.time()
 
-  GPIO.wait_for_edge(ECHO, GPIO.FALLING)
+  GPIO.wait_for_edge(ECHO, GPIO.BOTH, timeout=1000)
   end_time = time.time()
   
   total_time = end_time - start_time
   distance = total_time * 17150
   distance = round(distance, 2)
-  print "Distance: ", distance, "cm"
-  
+  if distance > 3 and distance < 400: 
+    print "Distance: ", distance, "cm"
+  else:
+    print "Out of Range"  
+
   cont = raw_input("Continue?(y/n): ")
     
 GPIO.cleanup()
