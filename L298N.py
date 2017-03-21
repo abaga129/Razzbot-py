96dd980ec44bb10b699718e06de7fe6e6a8cb8bc
Ethan Reker <ethanepr@hotmail.com>
2017-03-20 19:58:42 -0500

    Create L298N.py

---
+++ L298N.py
@@ -0,0 +1,7 @@
+####################################################################
+# L298N.py                                                         #
+# Created by Ethan Reker 2017                                      #
+# Module to simplify using the motor controller from the GPIO on   #
+# a raspberry pi                                                   #
+####################################################################
+
