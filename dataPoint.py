3d2da5c7316df9d0d665c41ace5338af164a31da
Ethan Reker <ethanepr@hotmail.com>
2017-04-10 18:39:07 -0500

    Update dataPoint.py

--- dataPoint.py
+++ dataPoint.py
@@ -1,2 +1,9 @@
+#
+# Data points will be captured periodically from sensors and this class
+# will be used to hold that information
 
 class DataPoint:
+  captureTime = 0
+  distance = 0
+  direction = ""
+  sensorId = ""
