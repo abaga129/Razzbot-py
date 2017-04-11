986b48c73ecae7f10f1b5fc457b4edc747a24bb6
Ethan Reker <ethanepr@hotmail.com>
2017-04-10 20:58:22 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -9,29 +9,63 @@ class LogicalUnit:
   motorController
   sensorData
   stopDistance = 20 #distance in centimeters
-  currentDirection = "N"
+  decisionList = []
   
   def readSensors(self):
     for sensor in sensorList:
       sensorData[sensor.name] = sensor.read()
       
   def start(self):
+    decision = ""
     while true:
-      if(sensorData["FRONT"] < 20 && sensorData["FRONT"] != 0):
-        motorController.setMode("STOP")
+      readSensors()
+      
+      #Check if front sensor is less than 20cm from an object
+      if(sensorData["FRONT"] < 20 and sensorData["FRONT"] != 0):
+        decision = "STOP"
+        motorController.setMode(decision)
+        
+        #If distance on left > distance on right
         if(sensorData["LEFT"] > sensorData["RIGHT"]):
           if(sensorData["LEFT"] != 0):
-            motorController.setMode("LEFT")
-            #Need to implement a way to turn for fixed time
+            decision = "LEFT"
+            motorController.setMode(decision)
+        
+        #If distance on right > distance on left
         else:
           if(sensorData["RIGHT"] != 0):
-            motorController.setMode("RIGHT")
-            
+            decision = "RIGHT"
+            motorController.setMode(decision)
       
-            
-    
-    
-    
-  
-  
-  
+      #Check if Left sensor reads less than 10cm  
+      elif(sensorData["LEFT"] < 10 and sensorData["LEFT"] != 0):
+        decision = "STOP"
+        motorController.setMode(decision)
+        #Check if right sensor reads greater than 10 : if so turn right
+        if(sensorData["RIGHT"] > 10 and sensorData["RIGHT"] != 0):
+          decision = "RIGHT"
+          motorController.setMode(decision) 
+        #Otherwise back up
+        else:
+          decision = "BACKWARD"
+          motorController.setMode(decision)
+      
+      #Check if Right sensor reads less than 10cm
+      elif(sensorData["RIGHT"] < 10 and sensorData["RIGHT"] != 0):
+        decision = "STOP"
+        motorController.setMode(decision)
+        if(sensorData["LEFT"] > 10):
+          decision = "LEFT"
+          motorController.setMode(decision)
+        else:
+          decision = "BACKWARD"
+          motorController.setMode(decision)
+          
+      else:
+        decision = "FORWARD"
+        motorController.setMode(decision)
+        
+      decisionList.append(decision)
+      
+      time.sleep(0.1)
+          
