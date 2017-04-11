f8fff15bd5c5b72fba69806563b486b6370073c8
Ethan Reker <ethanepr@hotmail.com>
2017-04-10 19:30:22 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -8,12 +8,27 @@ class LogicalUnit:
   sensorList = [] #A list that will contain all of the sensors
   motorController
   sensorData
+  stopDistance = 20 #distance in centimeters
+  currentDirection = "N"
   
-  def readSensors():
+  def readSensors(self):
     for sensor in sensorList:
-      sensorData[sensor.name].append(sensor.read())
+      sensorData[sensor.name] = sensor.read()
       
-  def turnLeft(degree):
+  def start(self):
+    while true:
+      if(sensorData["FRONT"] < 20 && sensorData["FRONT"] != 0):
+        motorController.setMode("STOP")
+        if(sensorData["LEFT"] > sensorData["RIGHT"]):
+          if(sensorData["LEFT"] != 0):
+            motorController.setMode("LEFT")
+            #Need to implement a way to turn for fixed time
+        else:
+          if(sensorData["RIGHT"] != 0):
+            motorController.setMode("RIGHT")
+            
+      
+            
     
     
     
