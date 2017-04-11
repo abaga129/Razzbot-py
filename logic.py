6f00960433e7c1b1afca8121a6b3d199fc307c33
Ethan Reker <ethanepr@hotmail.com>
2017-04-10 19:08:42 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,1 +1,22 @@
+######################################
+#
+#
+#
+######################################
 
+class LogicalUnit:
+  sensorList = [] #A list that will contain all of the sensors
+  motorController
+  sensorData
+  
+  def readSensors():
+    for sensor in sensorList:
+      sensorData[sensor.name].append(sensor.read())
+      
+  def turnLeft(degree):
+    
+    
+    
+  
+  
+  
