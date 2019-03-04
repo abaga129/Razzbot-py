0c550101984c16a34cc540b369200623e75ee0b8
abaga129 <ethanepr@hotmail.com>
2019-03-03 20:23:45 -0600

    remove sensor reading from dodge script for now

--- dodge.py
+++ dodge.py
@@ -6,7 +6,6 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-import config
 import HCSR04
 import L298N
 
@@ -21,34 +20,32 @@ def turn():
 
 stopCount = 0
 
-config.initialize()
+# sensor1 = HCSR04.HCSR04(37, 38, "FRONT")
 
-sensor1 = HCSR04.HCSR04(37, 38, "FRONT")
-
-sensor1.run()
+# sensor1.run()
 
 motor_ctrl = L298N.L298N(22, 11, 12, 13)
-motor_ctrl.start()
+motor_ctrl.run()
 
-try:
-  while True:
-	distance = sensor1.read()
-  print "FRONT: ", distance
-        time.sleep(0.05)
-        if distance < 40:
-            stopCount = stopCount + 1
-            print "STOP"
-            motor_ctrl.setMode("STOP")
-            if stopCount > 2:
-              turn()
-              stopCount = 0
-            time.sleep(2)
-        else:
-            print "FORWARD"
-            motor_ctrl.setMode("FORWARD")
-except KeyboardInterrupt:
-  print "Exiting program"
-  sys.exit()
+# try:
+#   while True:
+# 	distance = sensor1.read()
+#   print "FRONT: ", distance
+#         time.sleep(0.05)
+#         if distance < 40:
+#             stopCount = stopCount + 1
+#             print "STOP"
+#             motor_ctrl.setMode("STOP")
+#             if stopCount > 2:
+#               turn()
+#               stopCount = 0
+#             time.sleep(2)
+#         else:
+#             print "FORWARD"
+#             motor_ctrl.setMode("FORWARD")
+# except KeyboardInterrupt:
+#   print "Exiting program"
+#   sys.exit()
 
         
         
